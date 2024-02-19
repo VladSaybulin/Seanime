f837d7307a26241aea7f42d2f583fadca906794a
VladSaybulin <saybulinv@gmail.com>
2024-02-19 12:56:15 +0300

    Fix ksp plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,6 @@ plugins {
     alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.ksp) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
