9548cb5624587d8e993a7d70436af862687e7217
VladSaybulin <saybulinv@gmail.com>
2024-03-08 03:42:28 +0300

    Fix authorization header extension functions

--- core/network/src/main/java/ru/vladsaybulin/network/util/AuthorizationHeader.kt
+++ core/network/src/main/java/ru/vladsaybulin/network/util/AuthorizationHeader.kt
@@ -4,13 +4,14 @@ import okhttp3.Request
 
 internal fun Request.Builder.addAuthorizationHeader(accessToken: String): Request.Builder =
     this.apply {
-        addHeader(AUTHORIZATION_HEADER, "Bearer: $accessToken")
+        addHeader(AUTHORIZATION_HEADER, "$AUTHORIZATION_TYPE $accessToken")
     }
 
 internal fun Request.getAccessToken(): String? {
     val headerValue = header(AUTHORIZATION_HEADER) ?: return null
-    if (!headerValue.startsWith("Bearer: ")) return null
-    return headerValue.substring(8, headerValue.length)
+    if (!headerValue.startsWith(AUTHORIZATION_TYPE)) return null
+    return headerValue.substring(AUTHORIZATION_TYPE.length + 1, headerValue.length)
 }
 
+private const val AUTHORIZATION_TYPE = "Bearer"
 private const val AUTHORIZATION_HEADER = "Authorization"
