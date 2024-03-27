74d1d07901501fc586a712d69b4a3dea8686da26
VladSaybulin <saybulinv@gmail.com>
2024-03-28 01:17:19 +0300

    Create feature:userrate module

---
+++ feature/userrate/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.shikimori.android.feature)
+    alias(libs.plugins.shikimori.android.library.compose)
+    alias(libs.plugins.shikimori.android.hilt)
+}
+
+android {
+    namespace = "ru.vladsaybulin.feature.userrate"
+}
+
+dependencies {
+
+}
