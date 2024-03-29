dc954b2bda9abfeaddab2234b84036d84718db8e
VladSaybulin <saybulinv@gmail.com>
2024-03-29 04:25:55 +0300

    Fix UserRateLayout height. Add paddings in UserRateBottomSheetContent

--- feature/userrate/src/main/java/ru/vladsaybulin/feature/userrate/UserRate.kt
+++ feature/userrate/src/main/java/ru/vladsaybulin/feature/userrate/UserRate.kt
@@ -4,8 +4,11 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -16,6 +19,7 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,12 +28,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastForEachIndexed
 import androidx.compose.ui.util.fastMap
+import androidx.compose.ui.util.fastMaxBy
 import androidx.compose.ui.util.fastSumBy
 import kotlinx.datetime.Clock
 import ru.vladsaybulin.core.designsystem.theme.ShikimoriTheme
@@ -88,91 +94,96 @@ private fun UserRateBottomSheetContent(
         UserRateStatus.Dropped
     )
 
-    UserRateStatusLayout(
-        expandedButtons = expandedButtons,
-        selectedStatusIndex = statuses.indexOf(selectedStatus),
-        modifier = modifier,
-        statusButtons = {
-            statuses.forEach {
-                UserRateStatusButton(
-                    selected = it == selectedStatus,
-                    userRateStatus = it,
-                    onClick = {
-                        if (expandedButtons) {
-                            state.status = it
-                            expandedButtons = false
-                        } else {
-                            expandedButtons = true
+    Surface {
+        UserRateStatusLayout(
+            expandedButtons = expandedButtons,
+            selectedStatusIndex = statuses.indexOf(selectedStatus),
+            modifier = modifier,
+            statusButtons = {
+                statuses.forEach {
+                    UserRateStatusButton(
+                        selected = it == selectedStatus,
+                        userRateStatus = it,
+                        onClick = {
+                            if (expandedButtons) {
+                                state.status = it
+                                expandedButtons = false
+                            } else {
+                                expandedButtons = true
+                            }
+                        },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                }
+            }
+        ) {
+            Surface {
+                Column {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    ScoreRow(
+                        state = state.scoreState,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+                    Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+                        state.episodesCounterState?.let {
+                            Counter(
+                                state = it,
+                                label = { Text(text = stringResource(id = R.string.counter_label_episodes)) }
+                            )
+                        }
+
+                        state.chaptersCounterState?.let {
+                            Counter(
+                                state = it,
+                                label = { Text(text = stringResource(id = R.string.counter_label_chapters)) }
+                            )
+                        }
+
+                        state.volumesCounterState?.let {
+                            Counter(
+                                state = it,
+                                label = { Text(text = stringResource(id = R.string.counter_label_volumes)) }
+                            )
                         }
                     }
-                )
-            }
-        }
-    ) {
-        Surface {
-            Column {
-                ScoreRow(state = state.scoreState)
+                    Spacer(modifier = Modifier.height(16.dp))
 
-                Spacer(modifier = Modifier.height(16.dp))
+                    HorizontalDivider()
+                    BasicTextField(
+                        value = "",
+                        onValueChange = {},
+                        decorationBox = {
+                            Text(
+                                "Пара слов о тайтле",
+                                color = LocalContentColor.current.copy(alpha = 0.5f)
+                            )
+                            it()
+                        },
+                        minLines = 10,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                    )
+                    HorizontalDivider()
 
-                Row {
-                    state.episodesCounterState?.let {
-                        Counter(
-                            state = it,
-                            label = { Text(text = stringResource(id = R.string.counter_label_episodes)) }
-                        )
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = onSave,
+                        enabled = state.enabledSaveButton,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    ) {
+                        Text(text = stringResource(id = R.string.save))
                     }
 
-                    state.chaptersCounterState?.let {
-                        Counter(
-                            state = it,
-                            label = { Text(text = stringResource(id = R.string.counter_label_chapters)) }
-                        )
+                    TextButton(
+                        onClick = onDelete,
+                        colors = ButtonDefaults.textButtonColors(contentColor = ShikimoriTheme.colorScheme.error),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = stringResource(id = R.string.delete))
                     }
-
-                    state.volumesCounterState?.let {
-                        Counter(
-                            state = it,
-                            label = { Text(text = stringResource(id = R.string.counter_label_volumes)) }
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                HorizontalDivider()
-
-                BasicTextField(
-                    value = "",
-                    onValueChange = {},
-                    decorationBox = {
-                        Text(
-                            "Пара слов о тайтле",
-                            color = LocalContentColor.current.copy(alpha = 0.5f)
-                        )
-                        it()
-                    },
-                    minLines = 10
-                )
-
-                HorizontalDivider()
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(
-                    onClick = onSave,
-                    enabled = state.enabledSaveButton,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(text = stringResource(id = R.string.save))
-                }
-
-                TextButton(
-                    onClick = onDelete,
-                    colors = ButtonDefaults.textButtonColors(contentColor = ShikimoriTheme.colorScheme.error),
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                ) {
-                    Text(text = stringResource(id = R.string.delete))
                 }
             }
         }
@@ -189,6 +200,10 @@ fun UserRateStatusLayout(
 ) {
     val animateProgress by animateFloatAsState(targetValue = if (expandedButtons) 1f else 0f)
 
+    val density = LocalDensity.current
+
+    val bottomPaddingPx = WindowInsets.navigationBars.getBottom(density)
+
     SubcomposeLayout(modifier = modifier) { constraints ->
         val constraintsForButtons = constraints.copy(minHeight = 0)
         val buttonMeasurables = subcompose(UserRateStatusSlots.Buttons, statusButtons)
@@ -198,6 +213,9 @@ fun UserRateStatusLayout(
             buttonMeasurables.fastMap { it.measure(constraintsForButtons) }
         }
         val buttonsHeight = buttonPlaceables.fastSumBy { it.height }
+        val selectedButtonHeight = if (animateProgress > 0f) {
+            buttonPlaceables[selectedStatusIndex].height
+        } else buttonPlaceables.first().height
 
         val constraintsForContent = constraints.copy(minWidth = 0, minHeight = buttonsHeight)
         val contentMeasurables = subcompose(UserRateStatusSlots.Content, content)
@@ -205,8 +223,10 @@ fun UserRateStatusLayout(
             it.measure(constraintsForContent)
         }
 
+        val contentHeight = contentPlaceables.fastMaxBy { it.height }?.height ?: buttonsHeight
+
         val width = constraints.maxWidth
-        val height = contentPlaceables.maxOf { it.height }
+        val endHeight = contentHeight + selectedButtonHeight + bottomPaddingPx
 
         val initialY = if (animateProgress > 0f) {
             var selectedButtonOffset = 0f
@@ -216,11 +236,7 @@ fun UserRateStatusLayout(
             -selectedButtonOffset + animateProgress * selectedButtonOffset
         } else 0f
 
-        val selectedButtonHeight = if (animateProgress > 0f) {
-            buttonPlaceables[selectedStatusIndex].height
-        } else buttonPlaceables.first().height
-
-        layout(width, height) {
+        layout(width, endHeight) {
             var y = initialY
             buttonPlaceables.fastForEachIndexed { index, placeable ->
                 placeable.place(
@@ -231,7 +247,10 @@ fun UserRateStatusLayout(
             }
 
             contentPlaceables.fastForEach { placeable ->
-                placeable.placeRelative(x = 0, y = (selectedButtonHeight + (height - selectedButtonHeight) * animateProgress).roundToInt())
+                placeable.placeRelative(
+                    x = 0,
+                    y = (selectedButtonHeight + (contentHeight + bottomPaddingPx) * animateProgress).roundToInt()
+                )
             }
         }
     }
@@ -265,7 +284,10 @@ fun UserRateBottomSheetContentPreview() {
     val uiState = rememberUserRateUiState(setup = setup)
 
     ShikimoriTheme {
-        ModalBottomSheet(onDismissRequest = {  }) {
+        ModalBottomSheet(
+            sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
+            onDismissRequest = { }
+        ) {
             UserRateBottomSheetContent(
                 state = uiState,
                 onSave = { },
