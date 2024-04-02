88dbebec7655b447e86e4b6d063a2109fb7dfb72
VladSaybulin <saybulinv@gmail.com>
2024-04-02 20:28:27 +0300

    Launch authorization

--- app/src/main/java/ru/vladsaybulin/shikimori/MainActivity.kt
+++ app/src/main/java/ru/vladsaybulin/shikimori/MainActivity.kt
@@ -3,12 +3,10 @@ package ru.vladsaybulin.shikimori
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import dagger.hilt.android.AndroidEntryPoint
-import ru.vladsaybulin.core.auth.AuthorizationActivityResultCallback
+import ru.vladsaybulin.core.auth.AuthorizationLauncher
 import ru.vladsaybulin.core.auth.ShikimoriAuthState
-import ru.vladsaybulin.core.auth.createAuthorizationService
-import ru.vladsaybulin.core.auth.startAuthorizationActivity
+import ru.vladsaybulin.core.auth.registerAuthorizationLauncher
 import ru.vladsaybulin.shikimori.ui.App
 import javax.inject.Inject
 
@@ -17,19 +15,16 @@ class MainActivity : ComponentActivity() {
 
     @Inject lateinit var authState: ShikimoriAuthState
 
+    private lateinit var authorizationLauncher: AuthorizationLauncher
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val authorizationService = createAuthorizationService()
-        val authLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult(),
-            AuthorizationActivityResultCallback(authState, authorizationService)
-        )
+        authorizationLauncher = registerAuthorizationLauncher(authState)
 
         //if (!authState.isAuthorized) {
-        //    startAuthorizationActivity(authLauncher, authorizationService)
+        //    authorizationLauncher.launch()
         //}
-
 
         setContent {
             App()
