5c7034ed1c12e8f8f64424ca1dd71558a9d8efbe
VladSaybulin <saybulinv@gmail.com>
2024-04-02 18:57:50 +0300

    Add None in UserRateSetup and add UserRateEditorContext and enabledAutocorrect fields.  Rename AnimeUserRate to Success

--- feature/userrate/src/main/java/ru/vladsaybulin/feature/userrate/UserRateSetup.kt
+++ feature/userrate/src/main/java/ru/vladsaybulin/feature/userrate/UserRateSetup.kt
@@ -3,11 +3,11 @@ package ru.vladsaybulin.feature.userrate
 import ru.vladsaybulin.model.UserRate
 
 sealed interface UserRateSetup {
-    data class AnimeUserRate(
-        override val userRate: UserRate,
-        val maxEpisodes: Int,
-        val released: Boolean
+    data class Success(
+        val userRate: UserRate,
+        val context: UserRateEditorContext,
+        val enabledAutocorrect: Boolean
     ) : UserRateSetup
 
-    val userRate: UserRate
+    data object None : UserRateSetup
 }
