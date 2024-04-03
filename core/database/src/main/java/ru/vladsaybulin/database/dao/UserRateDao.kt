f09b2bb5949d089c8c7673308706d91b5497d287
VladSaybulin <saybulinv@gmail.com>
2024-04-03 13:49:03 +0300

    Create deleteUserRate method in UserRateDao

--- core/database/src/main/java/ru/vladsaybulin/database/dao/UserRateDao.kt
+++ core/database/src/main/java/ru/vladsaybulin/database/dao/UserRateDao.kt
@@ -2,6 +2,7 @@ package ru.vladsaybulin.database.dao
 
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import ru.vladsaybulin.database.models.UserRateDbo
 
 interface UserRateDao {
@@ -11,4 +12,8 @@ interface UserRateDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertOrReplaceUserRate(userRate: UserRateDbo)
+
+    @Query("DELETE FROM user_rates WHERE id = :userRateId")
+    suspend fun deleteUserRate(userRateId: Long)
+
 }
