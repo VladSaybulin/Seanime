aaefa24f842c2640884667b0ef48a9469bc300ce
VladSaybulin <saybulinv@gmail.com>
2024-04-03 04:26:15 +0300

    Create a UserRateApi and add an updateUserRate method to the UserRateDataSource

--- core/network/src/main/java/ru/vladsaybulin/network/datasource/UserRateDataSource.kt
+++ core/network/src/main/java/ru/vladsaybulin/network/datasource/UserRateDataSource.kt
@@ -1,13 +1,34 @@
 package ru.vladsaybulin.network.datasource
 
 import com.apollographql.apollo3.ApolloClient
+import retrofit2.Retrofit
+import retrofit2.http.Field
+import retrofit2.http.PATCH
+import retrofit2.http.Path
 import ru.vladsaybulin.common.network.NotFoundException
 import ru.vladsaybulin.core.network.graphql.AnimeUserRateQuery
+import ru.vladsaybulin.network.di.AuthorizedClient
+import ru.vladsaybulin.network.models.UserRateValuesDto
 import javax.inject.Inject
 import javax.inject.Singleton
 
+private interface UserRateApi {
+
+    @PATCH("/api/v2/user_rates/{id}")
+    suspend fun updateUserRate(
+        @Path("id") userRateId: Long,
+        @Field("user_rate") userRateValuesDto: UserRateValuesDto
+    )
+}
+
 @Singleton
-class UserRateDataSource @Inject constructor(private val apolloClient: ApolloClient) {
+class UserRateDataSource @Inject constructor(
+    private val apolloClient: ApolloClient,
+    @AuthorizedClient retrofit: Retrofit,
+) {
+
+    private val api = retrofit.create(UserRateApi::class.java)
+
     suspend fun getAnimeUserRate(animeId: Long): AnimeUserRateQuery.UserRate? {
         val response = apolloClient.query(AnimeUserRateQuery(id = animeId.toString())).execute()
         val anime = response.dataAssertNoErrors.animes.firstOrNull()
@@ -15,4 +36,7 @@ class UserRateDataSource @Inject constructor(private val apolloClient: ApolloCli
         return anime.userRate
     }
 
+    suspend fun updateUserRate(userRateId: Long, userRateValues: UserRateValuesDto) {
+        api.updateUserRate(userRateId, userRateValues)
+    }
 }
