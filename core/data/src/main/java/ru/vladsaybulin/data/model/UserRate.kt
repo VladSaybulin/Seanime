47d747f2dcf4cb8fd001f34d1f345ba41ca2b7af
VladSaybulin <saybulinv@gmail.com>
2024-04-03 13:38:46 +0300

    Create UserRateValues -> UserRateValuesDto mapper Create UserRateWithEntryLinkDto -> UserRateDbo mapper

--- core/data/src/main/java/ru/vladsaybulin/data/model/UserRate.kt
+++ core/data/src/main/java/ru/vladsaybulin/data/model/UserRate.kt
@@ -1,7 +1,12 @@
 package ru.vladsaybulin.data.model
 
 import ru.vladsaybulin.core.network.graphql.AnimeUserRateQuery
+import ru.vladsaybulin.database.models.UserRateDbo
+import ru.vladsaybulin.model.EntryType
 import ru.vladsaybulin.model.UserRate
+import ru.vladsaybulin.model.UserRateValues
+import ru.vladsaybulin.network.models.UserRateValuesDto
+import ru.vladsaybulin.network.models.UserRateWithEntryLinkDto
 
 fun AnimeUserRateQuery.UserRate.asUserRate() = UserRate(
     id = id,
@@ -14,4 +19,35 @@ fun AnimeUserRateQuery.UserRate.asUserRate() = UserRate(
     volumes = 0,
     rewatches = rewatches,
     text = text ?: ""
+)
+
+fun UserRateValues.asDto() = UserRateValuesDto(
+    status = status,
+    score = score,
+    episodes = episodes ?: 0,
+    chapters = chapters ?: 0,
+    volumes = volumes ?: 0,
+    rewatches = rewatches,
+    text = text
+)
+
+fun UserRateWithEntryLinkDto.asDbo() = UserRateDbo(
+    id = id,
+    animeId = when (entryType) {
+        EntryType.Anime -> entryId
+        else -> null
+    },
+    mangaId = when (entryType) {
+        EntryType.Anime -> null
+        else -> entryId
+    },
+    status = status,
+    score = score,
+    episodes = episodes,
+    chapters = chapters,
+    volumes = volumes,
+    rewatches = rewatches,
+    text = text,
+    createdAt = createdAt,
+    updatedAt = updatedAt
 )
