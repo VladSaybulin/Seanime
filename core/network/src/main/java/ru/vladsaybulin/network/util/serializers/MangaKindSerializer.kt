2b9db214872bede4e9fc31ea34a1419feba596b0
VladSaybulin <saybulinv@gmail.com>
2024-04-05 02:26:31 +0300

    Create MangaDto

---
+++ core/network/src/main/java/ru/vladsaybulin/network/util/serializers/MangaKindSerializer.kt
@@ -0,0 +1,24 @@
+package ru.vladsaybulin.network.util.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import ru.vladsaybulin.model.MangaKind
+import ru.vladsaybulin.model.asMangaKind
+
+class MangaKindSerializer : KSerializer<MangaKind> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
+        serialName = "manga_kind",
+        kind = PrimitiveKind.STRING
+    )
+
+    override fun deserialize(decoder: Decoder): MangaKind = decoder.decodeString().asMangaKind()
+
+    override fun serialize(encoder: Encoder, value: MangaKind) {
+        encoder.encodeString(value.serializedName)
+    }
+
+}
