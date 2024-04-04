1e0a45a5a12c31f75888e7d1b485e7ce3415b77a
VladSaybulin <saybulinv@gmail.com>
2024-04-05 02:31:26 +0300

    Create MangaRepository

---
+++ core/data/src/main/java/ru/vladsaybulin/data/repository/MangaRepository.kt
@@ -0,0 +1,22 @@
+package ru.vladsaybulin.data.repository
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import ru.vladsaybulin.common.network.Dispatcher
+import ru.vladsaybulin.common.network.ShikiDispatchers.IO
+import ru.vladsaybulin.data.model.asExternalModel
+import ru.vladsaybulin.data.model.asSimilarEntry
+import ru.vladsaybulin.model.MangaDetails
+import ru.vladsaybulin.model.SimilarEntry
+import ru.vladsaybulin.network.datasource.MangaDataSource
+import ru.vladsaybulin.network.models.MangaDto
+import javax.inject.Inject
+
+class MangaRepository @Inject constructor(
+    private val mangaDataSource: MangaDataSource,
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher
+) {
+
+}
