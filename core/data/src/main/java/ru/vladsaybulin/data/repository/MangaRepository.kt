af54c4b762faf7a5dae7aa8cc23b0e0240bde9a2
VladSaybulin <saybulinv@gmail.com>
2024-04-05 02:32:46 +0300

    Add methods to get manga details and similar

--- core/data/src/main/java/ru/vladsaybulin/data/repository/MangaRepository.kt
+++ core/data/src/main/java/ru/vladsaybulin/data/repository/MangaRepository.kt
@@ -18,5 +18,11 @@ class MangaRepository @Inject constructor(
     private val mangaDataSource: MangaDataSource,
     @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher
 ) {
+    fun geMangaDetails(mangaId: Long): Flow<MangaDetails> = flow {
+        emit(mangaDataSource.getMangaDetails(mangaId).asExternalModel())
+    }.flowOn(ioDispatcher)
 
+    fun getSimilarMangas(mangaId: Long): Flow<List<SimilarEntry>> = flow {
+        emit(mangaDataSource.getSimilarManga(mangaId).map(MangaDto::asSimilarEntry))
+    }.flowOn(ioDispatcher)
 }
