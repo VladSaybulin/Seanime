a1723514a51e92a1cbd414950196190aa4051408
VladSaybulin <saybulinv@gmail.com>
2024-04-04 19:37:25 +0300

    Create CreateUserRateDto and UserRateTargetTypeSerializer

---
+++ core/network/src/main/java/ru/vladsaybulin/network/models/CreateUserRateDto.kt
@@ -0,0 +1,26 @@
+package ru.vladsaybulin.network.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import ru.vladsaybulin.model.EntryType
+import ru.vladsaybulin.model.UserRateStatus
+import ru.vladsaybulin.network.util.serializers.UserRateStatusSerializer
+import ru.vladsaybulin.network.util.serializers.UserRateTargetTypeSerializer
+
+@Serializable
+class CreateUserRateDto(
+    @SerialName("user_id") val userId: Long,
+    @SerialName("target_type")
+    @Serializable(UserRateTargetTypeSerializer::class)
+    val targetType: EntryType,
+    @SerialName("target_id") val targetId: Long,
+    @SerialName("status")
+    @Serializable(UserRateStatusSerializer::class)
+    val status: UserRateStatus,
+    @SerialName("score") val score: Int,
+    @SerialName("episodes") val episodes: Int,
+    @SerialName("chapters") val chapters: Int,
+    @SerialName("volumes") val volumes: Int,
+    @SerialName("rewatches") val rewatches: Int,
+    @SerialName("text") val text: String
+)
---
+++ core/network/src/main/java/ru/vladsaybulin/network/util/serializers/UserRateTargetTypeSerializer.kt
@@ -0,0 +1,27 @@
+package ru.vladsaybulin.network.util.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import ru.vladsaybulin.model.EntryType
+import ru.vladsaybulin.model.asEntryType
+
+class UserRateTargetTypeSerializer : KSerializer<EntryType> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
+        serialName = "target_type",
+        kind = PrimitiveKind.STRING
+    )
+
+    override fun deserialize(decoder: Decoder): EntryType =
+        decoder.decodeString().asEntryType()
+
+    override fun serialize(encoder: Encoder, value: EntryType) {
+        encoder.encodeString(
+            value.serializedName.replaceFirstChar { it.uppercase() }
+        )
+    }
+
+}
