626a11c72073e4e9c30e02844fe1b684e5732e51
VladSaybulin <saybulinv@gmail.com>
2024-04-05 02:31:07 +0300

    Create mapper from MangaDto to SimilarEntry

--- core/data/src/main/java/ru/vladsaybulin/data/model/SimilarEntry.kt
+++ core/data/src/main/java/ru/vladsaybulin/data/model/SimilarEntry.kt
@@ -3,10 +3,19 @@ package ru.vladsaybulin.data.model
 import ru.vladsaybulin.model.EntryType
 import ru.vladsaybulin.model.SimilarEntry
 import ru.vladsaybulin.network.models.AnimeDto
+import ru.vladsaybulin.network.models.MangaDto
 
 fun AnimeDto.asSimilarEntry() = SimilarEntry(
     entryId = id,
     entryType = EntryType.Anime,
+    originalName = originalName,
+    russianName = russianName,
+    poster = poster?.asPoster()
+)
+
+fun MangaDto.asSimilarEntry() = SimilarEntry(
+    entryId = id,
+    entryType = EntryType.Manga,
     originalName = originalName,
     russianName = russianName,
     poster = poster?.asPoster()
