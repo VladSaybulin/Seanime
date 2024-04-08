51041bcc4a9ef79557f309fc788aef91523006bb
VladSaybulin <saybulinv@gmail.com>
2024-04-08 19:57:52 +0300

    Add parameters: modifier, nameTextStyle, bodyTextStyle

--- core/ui/src/main/java/ru/vladsaybulin/core/ui/entry/EntryRow.kt
+++ core/ui/src/main/java/ru/vladsaybulin/core/ui/entry/EntryRow.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +23,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -38,10 +40,13 @@ fun EntryRow(
     name: String,
     poster: Poster?,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
     userRateStatus: UserRateStatus = UserRateStatus.None,
     colors: EntryRowColors = EntryRowDefaults.colorsFor(userRateStatus),
     spacing: EntryRowSpacing = EntryRowDefaults.spacing(),
     posterWidth: Dp = EntryRowDefaults.NormalPosterWidth,
+    nameTextStyle: TextStyle = EntryRowDefaults.normalNameTextStyle,
+    bodyTextStyle: TextStyle = EntryRowDefaults.normalBodyTextStyle,
     border: BorderStroke? = null,
     shape: Shape = ShikimoriTheme.shapes.medium,
     posterShape: Shape = ShikimoriTheme.shapes.medium,
@@ -52,7 +57,8 @@ fun EntryRow(
         color = colors.containerColor,
         contentColor = colors.contentColor,
         border = border,
-        shape = shape
+        shape = shape,
+        modifier = modifier
     ) {
         Row(
             modifier = Modifier
@@ -77,7 +83,10 @@ fun EntryRow(
             Spacer(modifier = Modifier.width(spacing.posterSpace))
 
             Column {
-                EntryRowName(name = name)
+                EntryRowName(
+                    name = name,
+                    textStyle = nameTextStyle
+                )
                 if (details != null) {
                     Spacer(modifier = Modifier.height(spacing.detailsTopSpace))
                     Box(
@@ -85,7 +94,7 @@ fun EntryRow(
                             .clipToBounds()
                             .fillMaxSize()
                     ) {
-                        details()
+                        ProvideTextStyle(value = bodyTextStyle, content = details)
                     }
                 }
             }
@@ -96,13 +105,14 @@ fun EntryRow(
 @Composable
 private fun EntryRowName(
     name: String,
+    textStyle: TextStyle,
     modifier: Modifier = Modifier,
 ) {
     Text(
         text = name,
         maxLines = 2,
         overflow = TextOverflow.Ellipsis,
-        style = ShikimoriTheme.typography.titleMedium,
+        style = textStyle,
         modifier = modifier
     )
 }
@@ -184,6 +194,18 @@ object EntryRowDefaults {
     val NormalDetailsTopSpace = 4.dp
 
     val UserRateStatusBadgeSize = 24.dp
+
+    val SmallNameTextStyle
+        @Composable get() = ShikimoriTheme.typography.titleSmall
+
+    val normalNameTextStyle
+        @Composable get() = ShikimoriTheme.typography.titleMedium
+
+    val SmallBodyTextStyle
+        @Composable get() = ShikimoriTheme.typography.bodySmall
+
+    val normalBodyTextStyle
+        @Composable get() = ShikimoriTheme.typography.bodyMedium
 }
 
 @Preview
@@ -196,7 +218,10 @@ fun EntryRowPreview() {
             onClick = {},
             userRateStatus = UserRateStatus.Planned
         ) {
-            Box(Modifier.fillMaxSize().background(Color.Red))
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Red))
         }
     }
 }
