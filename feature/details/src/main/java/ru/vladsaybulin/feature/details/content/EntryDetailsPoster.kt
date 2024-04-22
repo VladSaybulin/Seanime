f0577c8a4f63c7e398622853824884c192300e6a
VladSaybulin <saybulinv@gmail.com>
2024-04-22 10:31:17 +0300

    Now background and poster use the same painter in details screen

--- feature/details/src/main/java/ru/vladsaybulin/feature/details/content/EntryDetailsPoster.kt
+++ feature/details/src/main/java/ru/vladsaybulin/feature/details/content/EntryDetailsPoster.kt
@@ -1,8 +1,10 @@
 package ru.vladsaybulin.feature.details.content
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListScope
@@ -10,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -19,9 +20,8 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import ru.vladsaybulin.core.designsystem.theme.ShikimoriTheme
-import ru.vladsaybulin.core.ui.EntryPoster
 import ru.vladsaybulin.core.ui.R
 import ru.vladsaybulin.core.ui.drawForegroundGradientScrim
 import ru.vladsaybulin.model.common.Image
@@ -48,19 +48,38 @@ private fun EntryDetailsPoster(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier.fillMaxWidth()) {
-        if (poster != null) {
-            PosterBackground(
-                poster = poster,
-                modifier = Modifier.matchParentSize()
+
+        val painter = if (poster == null || LocalInspectionMode.current) {
+            painterResource(id = R.drawable.no_poster)
+        } else {
+            rememberAsyncImagePainter(
+                model = poster.originalUrl,
+                contentScale = ContentScale.Crop
             )
         }
 
-        EntryPoster(
-            poster = poster,
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .fillMaxWidth(0.7f)
-                .align(Alignment.TopCenter)
+                .matchParentSize()
+                //.blur(16.dp)
+                .drawForegroundGradientScrim(
+                    startColor = ShikimoriTheme.colorScheme.surface.copy(alpha = 0.8f),
+                    stopColor = ShikimoriTheme.colorScheme.surface
+                )
+        )
+
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
                 .padding(top = topSpaceDp)
+                .fillMaxWidth(0.7f)
+                .aspectRatio(3 / 4f)
+                .align(Alignment.TopCenter)
                 .shadow(4.dp, shape = ShikimoriTheme.shapes.large, clip = true)
                 .clickable(
                     interactionSource = remember { MutableInteractionSource() },
@@ -68,31 +87,9 @@ private fun EntryDetailsPoster(
                     enabled = poster != null,
                     role = Role.Image,
                     onClick = onPosterClick
-
                 )
         )
     }
-}
-
-@Composable
-private fun PosterBackground(
-    poster: Image,
-    modifier: Modifier = Modifier
-) {
-    AsyncImage(
-        model = poster.originalUrl,
-        contentDescription = null,
-        modifier = modifier
-            .blur(16.dp)
-            .drawForegroundGradientScrim(
-                startColor = ShikimoriTheme.colorScheme.surface.copy(alpha = 0.5f),
-                stopColor = ShikimoriTheme.colorScheme.surface
-            ),
-        placeholder = if (LocalInspectionMode.current) {
-            painterResource(id = R.drawable.preview_poster_1)
-        } else null,
-        contentScale = ContentScale.Crop
-    )
 }
 
 @Composable
