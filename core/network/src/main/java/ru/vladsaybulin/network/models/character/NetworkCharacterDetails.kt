3c6d1b58c9ecebb965d0dc8af78e21671a374462
VladSaybulin <saybulinv@gmail.com>
2024-04-27 17:25:42 +0300

    Create network model: NetworkCharacterDetails

---
+++ core/network/src/main/java/ru/vladsaybulin/network/models/character/NetworkCharacterDetails.kt
@@ -0,0 +1,57 @@
+package ru.vladsaybulin.network.models.character
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import ru.vladsaybulin.network.models.NetworkAnime
+import ru.vladsaybulin.network.models.NetworkManga
+import ru.vladsaybulin.network.models.common.NetworkImage
+import ru.vladsaybulin.network.models.person.NetworkPerson
+import ru.vladsaybulin.network.util.serializers.ImageSerializer
+
+@Serializable
+data class NetworkCharacterDetails(
+
+    @SerialName("id")
+    val id: Long,
+
+    @SerialName("name")
+    val name: String,
+
+    @SerialName("russian")
+    val nameRu: String?,
+
+    @Serializable(ImageSerializer::class)
+    @SerialName("image")
+    val poster: NetworkImage?,
+
+    @SerialName("altname")
+    val alternativeName: String?,
+
+    @SerialName("japanese")
+    val nameJp: String?,
+
+    @SerialName("description_html")
+    val descriptionHtml: String?,
+
+    @SerialName("description_source")
+    val descriptionSource: String?,
+
+    @SerialName("favoured")
+    val favoured: Boolean,
+
+    @SerialName("topic_id")
+    val topicId: Long?,
+
+    @SerialName("updated_at")
+    val updatedAt: Instant,
+
+    @SerialName("seyu")
+    val seyu: List<NetworkPerson>,
+
+    @SerialName("anime")
+    val animes: List<NetworkAnime>,
+
+    @SerialName("mangas")
+    val mangas: List<NetworkManga>
+)
