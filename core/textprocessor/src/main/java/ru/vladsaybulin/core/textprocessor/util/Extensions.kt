c1c7aa411008b76666425cb075cdc5e19de1cfd7
VladSaybulin <saybulinv@gmail.com>
2024-05-05 08:43:45 +0300

    Delete TagNode.rawBody extension Move TagNode.htmlClasses() extension to Extensions.kt file

--- core/textprocessor/src/main/java/ru/vladsaybulin/core/textprocessor/util/Extensions.kt
+++ core/textprocessor/src/main/java/ru/vladsaybulin/core/textprocessor/util/Extensions.kt
@@ -1,7 +1,11 @@
 package ru.vladsaybulin.core.textprocessor.util
 
 import org.primeframework.transformer.domain.Document
+import org.primeframework.transformer.domain.TagNode
 import org.primeframework.transformer.service.HTMLParser
 
 fun String.toHtmlDocument(): Document =
     HTMLParser().buildDocument(this, emptyMap())
+
+fun TagNode.htmlClasses() =
+    attributes["class"]?.split(' ')
