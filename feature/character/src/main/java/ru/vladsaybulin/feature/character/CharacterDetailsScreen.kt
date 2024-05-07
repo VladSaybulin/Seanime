1c0018c10677e0dc066b7a086bd6f0248ca4a653
VladSaybulin <saybulinv@gmail.com>
2024-05-08 00:45:59 +0300

    Use ShikimoriExpandableText on character details screen

--- feature/character/src/main/java/ru/vladsaybulin/feature/character/CharacterDetailsScreen.kt
+++ feature/character/src/main/java/ru/vladsaybulin/feature/character/CharacterDetailsScreen.kt
@@ -1,6 +1,7 @@
 package ru.vladsaybulin.feature.character
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,28 +22,35 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import androidx.compose.ui.util.fastForEach
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.rememberAsyncImagePainter
 import kotlinx.datetime.Clock
+import ru.vladsaybulin.core.designsystem.components.ShikimoriExpandableText
 import ru.vladsaybulin.core.designsystem.theme.ShikimoriTheme
 import ru.vladsaybulin.core.navigation.NavigationEvent
-import ru.vladsaybulin.core.ui.AnnotatedText
 import ru.vladsaybulin.core.ui.LocalScreenContentPadding
+import ru.vladsaybulin.core.ui.toComposeAnnotatedString
 import ru.vladsaybulin.model.annotatedtext.AnnotatedText
 import ru.vladsaybulin.model.character.CharacterDetails
+import ru.vladsaybulin.model.common.EntryType
 import ru.vladsaybulin.model.common.Image
 import ru.vladsaybulin.core.ui.R as coreUiR
 
@@ -109,13 +117,62 @@ fun CharacterDetailsContent(
 
         if (details.description != null) {
             item {
-                AnnotatedText(
-                    text = details.description!!,
-                    style = ShikimoriTheme.typography.bodySmall,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                ) { _, _ -> }
+                CharacterDescription(details.description!!, onNavigationEvent)
             }
         }
+    }
+}
+
+@Composable
+fun CharacterDescription(
+    description: AnnotatedText,
+    onNavigationEvent: (NavigationEvent) -> Unit
+) {
+    val (expanded, setExpanded) = remember { mutableStateOf(false) }
+    val layoutResult  = remember { mutableStateOf<TextLayoutResult?>(null) }
+
+    val annotatedString = description.toComposeAnnotatedString()
+
+    val pressIndicator = Modifier.pointerInput(Unit) {
+        detectTapGestures { pos ->
+            layoutResult.value?.let { layoutResult ->
+                val offset = layoutResult.getOffsetForPosition(pos)
+                annotatedString.getStringAnnotations(offset, offset)
+                    .fastForEach {
+                        handleDescriptionClick(
+                            tag = it.tag,
+                            annotation = it.item,
+                            onNavigationEvent = onNavigationEvent
+                        )
+                    }
+            }
+        }
+    }
+
+    ShikimoriExpandableText(
+        text = annotatedString,
+        expanded = expanded,
+        onExpandedChange = setExpanded,
+        style = ShikimoriTheme.typography.bodyMedium,
+        modifier = Modifier.padding(horizontal = 16.dp),
+        textModifier = pressIndicator,
+        onTextLayout = { layoutResult.value = it }
+    )
+}
+
+fun handleDescriptionClick(
+    tag: String,
+    annotation: String,
+    onNavigationEvent: (NavigationEvent) -> Unit
+) {
+    val navEventOrNull = when (tag) {
+        "anime" -> NavigationEvent.EntryDetails(EntryType.Anime, annotation.toLong())
+        "manga", "ranobe" -> NavigationEvent.EntryDetails(EntryType.Manga, annotation.toLong())
+        "character", -> NavigationEvent.CharacterDetails(annotation.toLong())
+        else -> null
+    }
+    if (navEventOrNull != null) {
+        onNavigationEvent(navEventOrNull)
     }
 }
 
