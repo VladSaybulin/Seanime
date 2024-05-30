b98397edae5b56520462eb6949b4cc626952eabe
VladSaybulin <saybulinv@gmail.com>
2024-05-30 14:46:28 +0300

    Add missing values to RelationType and remove serializedValue field

--- core/model/src/main/java/ru/vladsaybulin/model/related/RelationType.kt
+++ core/model/src/main/java/ru/vladsaybulin/model/related/RelationType.kt
@@ -1,18 +1,16 @@
 package ru.vladsaybulin.model.related
 
-enum class RelationType(val serializedName: String) {
-    Adaptation("Adaptation"),
-    AltHistory("Alternative version"),
-    SideStory("Side story"),
-    SpinOff("Spin-off"),
-    Sequel("Sequel"),
-    Prequel("Prequel"),
-    Summary("Summary"),
-    Character("Character"),
-    Other("Other")
-}
-
-fun String?.asRelationType() = when (this) {
-    null -> RelationType.Other
-    else -> RelationType.entries.firstOrNull { it.serializedName == this } ?: RelationType.Other
+enum class RelationType {
+    Adaptation,
+    AltSetting,
+    AltHistory,
+    Character,
+    FullStory,
+    ParentStory,
+    Sequel,
+    Prequel,
+    SideStory,
+    SpinOff,
+    Summary,
+    Other
 }
