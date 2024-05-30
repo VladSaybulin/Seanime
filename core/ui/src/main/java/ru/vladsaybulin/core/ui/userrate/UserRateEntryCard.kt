8a0ec7ce6893c5bf98dee6e8da75d3d87576e33e
VladSaybulin <saybulinv@gmail.com>
2024-05-30 19:35:21 +0300

    Use SeanimeUserRateLinearProgressIndicator in UserRateEntryCard Delete UserRateProgressLimit

--- core/ui/src/main/java/ru/vladsaybulin/core/ui/userrate/UserRateEntryCard.kt
+++ core/ui/src/main/java/ru/vladsaybulin/core/ui/userrate/UserRateEntryCard.kt
@@ -7,18 +7,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import ru.vladsaybulin.core.designsystem.components.SeanimeUserRateLinearProgressIndicator
 import ru.vladsaybulin.core.designsystem.icons.ShikimoriIcons
 import ru.vladsaybulin.core.designsystem.theme.SeanimeTheme
 import ru.vladsaybulin.core.ui.R
@@ -29,16 +27,12 @@ import ru.vladsaybulin.core.ui.score.SmallStarSize
 import ru.vladsaybulin.core.ui.strings.animeKindString
 import ru.vladsaybulin.core.ui.strings.mangaKindString
 import ru.vladsaybulin.model.anime.Anime
-import ru.vladsaybulin.model.common.EntryStatus.Ongoing
 import ru.vladsaybulin.model.common.Image
 import ru.vladsaybulin.model.manga.Manga
-import ru.vladsaybulin.model.userrate.UserRateProgressLimit
-import ru.vladsaybulin.model.userrate.UserRateProgressLimit.Companion.Unlimited
 import ru.vladsaybulin.model.userrate.UserRateStatus.None
 import ru.vladsaybulin.model.userrate.UserRateStatus.Rewatching
 import ru.vladsaybulin.model.userrate.UserRateStatus.Watching
 import ru.vladsaybulin.model.userrate.UserRateWithEntry
-import ru.vladsaybulin.model.userrate.toLimit
 
 @Composable
 fun UserRateEntryCard(
@@ -53,9 +47,9 @@ fun UserRateEntryCard(
 
     val name: String
     val poster: Image?
-    val episodesLimit: UserRateProgressLimit?
-    val chaptersLimit: UserRateProgressLimit?
-    val volumesLimit: UserRateProgressLimit?
+    val episodesState: UserRateProgressState?
+    val chaptersState: UserRateProgressState?
+    val volumesState: UserRateProgressState?
     val kindString: String?
     val airedInYear: Int?
 
@@ -66,14 +60,13 @@ fun UserRateEntryCard(
 
         name = anime.russianName ?: anime.name
         poster = anime.poster
-        episodesLimit = when {
-            !inProgress || anime.episodes == 1 -> null
-            anime.episodes > 0 -> anime.episodes.toLimit()
-            anime.status == Ongoing && anime.episodesAired > 0 -> anime.episodesAired.toLimit()
-            else -> null
-        }
-        volumesLimit = null
-        chaptersLimit = null
+        episodesState = UserRateProgressState(
+            progress = userRate.episodes,
+            available = anime.episodesAired,
+            max = anime.episodes
+        )
+        volumesState = null
+        chaptersState = null
         kindString = animeKindString(anime.kind)
         airedInYear = anime.airedOn?.year
     } else if (userRateWithEntry.manga != null) {
@@ -81,13 +74,21 @@ fun UserRateEntryCard(
 
         name = manga.russianName ?: manga.name
         poster = manga.poster
-        episodesLimit = null
-        volumesLimit = if (inProgress && manga.chapters == 0 && manga.volumes > 0) {
-            UserRateProgressLimit(manga.volumes)
+        episodesState = null
+        volumesState = if (inProgress && manga.chapters == 0 && manga.volumes > 0) {
+            UserRateProgressState(
+                progress = userRate.volumes,
+                available = manga.volumes,
+                max = manga.volumes
+            )
         } else null
-        chaptersLimit = if (inProgress && manga.chapters > 0) {
-            UserRateProgressLimit(manga.chapters)
-        } else Unlimited
+        chaptersState = if (volumesState == null) {
+            UserRateProgressState(
+                progress = userRate.chapters,
+                available = manga.chapters,
+                max = manga.chapters
+            )
+        } else null
         kindString = mangaKindString(manga.kind)
         airedInYear = manga.airedOn?.year
     } else throw IllegalArgumentException()
@@ -109,7 +110,9 @@ fun UserRateEntryCard(
         modifier = modifier
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth().weight(1f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Column(Modifier.weight(1f)) {
@@ -126,23 +129,20 @@ fun UserRateEntryCard(
                 Spacer(modifier = Modifier.height(8.dp))
 
                 when {
-                    episodesLimit != null -> ProgressIndicator(
-                        progress = userRate.episodes,
-                        limit = episodesLimit,
+                    episodesState != null -> ProgressIndicator(
+                        state = episodesState,
                         unlimitedProgressStringRes = R.string.episodes_progress,
                         limitedProgressStringRes = R.string.episodes_progress_of_limit
                     )
 
-                    volumesLimit != null -> ProgressIndicator(
-                        progress = userRate.volumes,
-                        limit = volumesLimit,
+                    volumesState != null -> ProgressIndicator(
+                        state = volumesState,
                         unlimitedProgressStringRes = R.string.volumes_progress,
                         limitedProgressStringRes = R.string.volumes_progress_of_limit
                     )
 
-                    chaptersLimit != null -> ProgressIndicator(
-                        progress = userRate.chapters,
-                        limit = chaptersLimit,
+                    chaptersState != null -> ProgressIndicator(
+                        state = chaptersState,
                         unlimitedProgressStringRes = R.string.chapters_progress,
                         limitedProgressStringRes = R.string.chapters_progress_of_limit
                     )
@@ -165,26 +165,54 @@ fun UserRateEntryCard(
 
 @Composable
 private fun ProgressIndicator(
-    progress: Int,
-    limit: UserRateProgressLimit,
+    state: UserRateProgressState,
     unlimitedProgressStringRes: Int,
     limitedProgressStringRes: Int,
 ) {
     Column {
-        if (limit != Unlimited) {
-            LinearProgressIndicator(
-                progress = { progress / limit.limit.toFloat() },
-                modifier = Modifier.clip(CircleShape)
+
+        if (state.isProgressIndicatorVisible()) {
+            SeanimeUserRateLinearProgressIndicator(
+                progress = state.progressFraction,
+                availableProgress = state.availableFraction
             )
             Spacer(modifier = Modifier.height(2.dp))
         }
+
         Text(
-            text = if (limit == Unlimited) {
-                stringResource(id = unlimitedProgressStringRes, progress)
+            text = if (state.max == 0) {
+                stringResource(id = unlimitedProgressStringRes, state.progress)
             } else {
-                stringResource(id = limitedProgressStringRes, progress, limit.limit)
+                stringResource(id = limitedProgressStringRes, state.progress, state.max)
             },
             style = SeanimeTheme.typography.bodySmall
         )
     }
+}
+
+private class UserRateProgressState(
+    val progress: Int,
+    val available: Int,
+    max: Int
+) {
+    val max = when {
+        max != 0 -> max
+        available != 0 -> available
+        progress != 0 -> progress
+        else -> 0
+    }
+
+    val progressFraction: Float
+        get() {
+            require(isProgressIndicatorVisible())
+            return progress / max.toFloat()
+        }
+
+    val availableFraction: Float
+        get() {
+            require(isProgressIndicatorVisible())
+            return available / max.toFloat()
+        }
+
+    fun isProgressIndicatorVisible() = max != 0
 }
