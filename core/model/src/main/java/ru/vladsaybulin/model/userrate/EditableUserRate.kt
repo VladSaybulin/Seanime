4005c8f9bcdc73d96fa23598865276bbd9a0d844
VladSaybulin <saybulinv@gmail.com>
2024-07-23 21:48:13 +0300

    Add docs for EditableUserRate class

--- core/model/src/main/java/ru/vladsaybulin/model/userrate/EditableUserRate.kt
+++ core/model/src/main/java/ru/vladsaybulin/model/userrate/EditableUserRate.kt
@@ -3,6 +3,9 @@ package ru.vladsaybulin.model.userrate
 import ru.vladsaybulin.model.common.EntryStatus
 import ru.vladsaybulin.model.common.EntryType
 
+/**
+ * A original [userRate] with additional information for editing
+ */
 data class EditableUserRate(
     val userRate: UserRate,
     val titleType: EntryType,
