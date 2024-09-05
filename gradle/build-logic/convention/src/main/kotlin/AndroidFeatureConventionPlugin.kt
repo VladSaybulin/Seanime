a0cdeca1308ce6440a10f9364e7151deb5238c4e
VladSaybulin <saybulinv@gmail.com>
2024-09-05 14:23:31 +0300

    Include navigation compose in seanime.android.feature

--- gradle/build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -20,6 +20,7 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 "implementation"(project(":core:model"))
                 "implementation"(project(":core:ui"))
 
+                "implementation"(libs.findLibrary("androidx.navigation.compose").get())
                 "implementation"(libs.findLibrary("androidx.hilt.navigation.compose").get())
                 "implementation"(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
                 "implementation"(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
