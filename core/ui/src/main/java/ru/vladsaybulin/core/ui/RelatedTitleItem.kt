8426babfa2bf8a45e2a6b12a1df87a7080573145
VladSaybulin <saybulinv@gmail.com>
2024-09-05 14:22:48 +0300

    Create shared RelatedTitleItem ui component

---
+++ core/ui/src/main/java/ru/vladsaybulin/core/ui/RelatedTitleItem.kt
@@ -0,0 +1,146 @@
+package ru.vladsaybulin.core.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import ru.vladsaybulin.core.designsystem.components.ShikimoriTextBadge
+import ru.vladsaybulin.core.ui.anime.AnimeInfoKindAndYearText
+import ru.vladsaybulin.core.ui.entry.EntryInfoScore
+import ru.vladsaybulin.core.ui.entry.EntryListItem
+import ru.vladsaybulin.core.ui.manga.MangaInfoKindAndYearText
+import ru.vladsaybulin.model.anime.Anime
+import ru.vladsaybulin.model.common.EntryStatus
+import ru.vladsaybulin.model.common.EntryType
+import ru.vladsaybulin.model.manga.Manga
+import ru.vladsaybulin.model.related.RelatedEntry
+import ru.vladsaybulin.model.related.RelationType
+
+@Composable
+fun RelatedTitleItem(
+    relatedTitle: RelatedEntry,
+    onClick: (EntryType, Long) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    if (relatedTitle.anime != null) {
+        val nonNullAnime = checkNotNull(relatedTitle.anime)
+        RelatedAnimeItem(
+            anime = nonNullAnime,
+            relationType = relatedTitle.relationType,
+            onClick = { onClick(EntryType.Anime, nonNullAnime.id) },
+            modifier = modifier
+        )
+    } else {
+        val nonNullManga = checkNotNull(relatedTitle.manga)
+        RelatedMangaItem(
+            manga = nonNullManga,
+            relationType = relatedTitle.relationType,
+            onClick = { onClick(EntryType.Manga, nonNullManga.id) },
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+private fun RelatedAnimeItem(
+    anime: Anime,
+    relationType: RelationType,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    EntryListItem(
+        name = anime.run { russianName ?: name },
+        imageUrl = anime.poster?.previewUrl,
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        imageWidth = 72.dp,
+        imageIgnoresPadding = true,
+        metadata = {
+            AnimeInfoKindAndYearText(kind = anime.kind, year = anime.airedOn?.year)
+
+            Row {
+                if (anime.status != EntryStatus.None) {
+                    EntryStatusBadge(
+                        status = anime.status,
+                        modifier = Modifier.padding(
+                            top = 2.dp,
+                            bottom = 2.dp,
+                            end = 4.dp
+                        )
+                    )
+                }
+
+                ShikimoriTextBadge {
+                    Text(relationTypeString(relationType))
+                }
+            }
+
+            EntryInfoScore(score = anime.score ?: 0f)
+        }
+    )
+}
+
+@Composable
+private fun RelatedMangaItem(
+    manga: Manga,
+    relationType: RelationType,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    EntryListItem(
+        name = manga.run { russianName ?: name },
+        imageUrl = manga.poster?.previewUrl,
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        imageWidth = 72.dp,
+        imageIgnoresPadding = true,
+        metadata = {
+            MangaInfoKindAndYearText(kind = manga.kind, year = manga.airedOn?.year)
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (manga.status != EntryStatus.None) {
+                    EntryStatusBadge(
+                        status = manga.status,
+                        modifier = Modifier.padding(
+                            top = 2.dp,
+                            bottom = 2.dp,
+                            end = 4.dp
+                        ),
+                    )
+                }
+
+                ShikimoriTextBadge {
+                    Text(relationTypeString(relationType))
+                }
+            }
+
+            EntryInfoScore(score = manga.score ?: 0f)
+        }
+    )
+}
+
+@Composable
+@ReadOnlyComposable
+private fun relationTypeString(relationType: RelationType) =
+    stringResource(id = relationTypeStringResId(relationType))
+
+private fun relationTypeStringResId(relationType: RelationType) = when (relationType) {
+    RelationType.Adaptation -> R.string.core_ui_relation_type_adaptation
+    RelationType.AltSetting -> R.string.core_ui_relation_type_alt_setting
+    RelationType.AltHistory -> R.string.core_ui_relation_type_alt_history
+    RelationType.SideStory -> R.string.core_ui_relation_type_side_story
+    RelationType.FullStory -> R.string.core_ui_relation_type_full_story
+    RelationType.ParentStory -> R.string.core_ui_relation_type_parent_history
+    RelationType.Sequel -> R.string.core_ui_relation_type_sequel
+    RelationType.Prequel -> R.string.core_ui_relation_type_prequel
+    RelationType.Summary -> R.string.core_ui_relation_type_summary
+    RelationType.Character -> R.string.core_ui_relation_type_character
+    RelationType.SpinOff -> R.string.core_ui_relation_type_spin_off
+    RelationType.Other -> R.string.core_ui_relation_type_other
+}
