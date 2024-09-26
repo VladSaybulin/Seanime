7d897023cf72bfcb52cc4c303aa9437d5be5ed83
VladSaybulin <saybulinv@gmail.com>
2024-09-26 14:42:24 +0300

    Fix serializedValue in Order

--- core/model/src/main/java/ru/vladsaybulin/model/search/Order.kt
+++ core/model/src/main/java/ru/vladsaybulin/model/search/Order.kt
@@ -3,8 +3,8 @@ package ru.vladsaybulin.model.search
 enum class Order (val serializedValue: String) {
     Popularity("popularity"),
     Ranked("ranked"),
-    Alphabet("alphabet"),
-    Created("created"),
-    CreatedDesc("created_decs"),
+    Alphabet("name"),
+    Created("created_at"),
+    CreatedDesc("created_at_desc"),
     Random("ranked_random"),
 }
