70b7b384b9b1f0dd99d93d7175c1d7c77eabb9f8
VladSaybulin <saybulinv@gmail.com>
2026-01-31 23:14:59 +0300

    Add :core:ui2:strings

---
+++ core/ui2/strings/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.seanime.android.library)
+    alias(libs.plugins.seanime.android.library.compose)
+}
+
+android {
+    namespace = "ru.vladsaybulin.core.ui2.strings"
+}
+
+dependencies {
+    implementation(projects.core.model)
+
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.androidx.compose.runtime)
+}
