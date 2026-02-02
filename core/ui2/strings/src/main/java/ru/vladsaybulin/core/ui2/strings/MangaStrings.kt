a3989efaf62cfd1b9799d8d5bcf9f42f1b931235
VladSaybulin <saybulinv@gmail.com>
2026-02-02 03:03:11 +0300

    Implement MangaStrings

--- core/ui2/strings/src/main/java/ru/vladsaybulin/core/ui2/strings/MangaStrings.kt
+++ core/ui2/strings/src/main/java/ru/vladsaybulin/core/ui2/strings/MangaStrings.kt
@@ -1,10 +1,41 @@
 package ru.vladsaybulin.core.ui2.strings
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.res.stringResource
 import ru.vladsaybulin.model.common.EntryStatus
+import ru.vladsaybulin.model.manga.MangaKind
 import ru.vladsaybulin.model.userrate.UserRateStatus
 
-object MangaStrings : TitleRelatedStrings {
-    override fun titleStatus(status: EntryStatus): Int = TODO("Not yet implemented")
+object MangaStrings : DependsOnTitleStrings {
 
-    override fun userStatus(status: UserRateStatus): Int = TODO("Not yet implemented")
+    override fun titleStatusId(status: EntryStatus): Int = when (status) {
+        EntryStatus.Anons -> R.string.core_ui2_strings_manga_status_anons
+        EntryStatus.Ongoing -> R.string.core_ui2_strings_manga_status_ongoing
+        EntryStatus.Released -> R.string.core_ui2_strings_manga_status_released
+        EntryStatus.Paused -> R.string.core_ui2_strings_manga_status_paused
+        EntryStatus.Discontinued -> R.string.core_ui2_strings_manga_status_discontinued
+        EntryStatus.None -> error("EntryStatus.None is not supported")
+    }
+
+    override fun userStatusId(status: UserRateStatus): Int = when (status) {
+        UserRateStatus.Planned -> R.string.core_ui2_strings_manga_user_status_planned
+        UserRateStatus.Watching -> R.string.core_ui2_strings_manga_user_status_watching
+        UserRateStatus.Rewatching -> R.string.core_ui2_strings_manga_user_status_rewatching
+        UserRateStatus.Completed -> R.string.core_ui2_strings_manga_user_status_completed
+        UserRateStatus.OnHold -> R.string.core_ui2_strings_manga_user_status_on_hold
+        UserRateStatus.Dropped -> R.string.core_ui2_strings_manga_user_status_dropped
+        UserRateStatus.None -> error("UserRateStatus.None is not supported")
+    }
+
+    fun kindId(kind: MangaKind): Int = when (kind) {
+        MangaKind.Manga -> R.string.core_ui2_strings_manga_kind_manga
+        MangaKind.Manhwa -> R.string.core_ui2_strings_manga_kind_manhwa
+        MangaKind.Manhua -> R.string.core_ui2_strings_manga_kind_manhua
+        MangaKind.OneShot -> R.string.core_ui2_strings_manga_kind_oneshot
+        MangaKind.Doujin -> R.string.core_ui2_strings_manga_kind_doujin
+        MangaKind.LightNovel -> R.string.core_ui2_strings_manga_kind_light_novel
+        MangaKind.Novel -> R.string.core_ui2_strings_manga_kind_novel
+        MangaKind.None -> error("MangaKind.None is not supported")
+    }
 }
