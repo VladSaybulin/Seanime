61de79e37f2713deb99ef0d54153276a4d1b1a4b
VladSaybulin <saybulinv@gmail.com>
2026-02-02 03:02:02 +0300

    Implement AnimeStrings

--- core/ui2/strings/src/main/java/ru/vladsaybulin/core/ui2/strings/AnimeStrings.kt
+++ core/ui2/strings/src/main/java/ru/vladsaybulin/core/ui2/strings/AnimeStrings.kt
@@ -1,10 +1,53 @@
 package ru.vladsaybulin.core.ui2.strings
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.res.stringResource
+import ru.vladsaybulin.model.anime.AnimeKind
+import ru.vladsaybulin.model.anime.AnimeRating
 import ru.vladsaybulin.model.common.EntryStatus
 import ru.vladsaybulin.model.userrate.UserRateStatus
 
-object AnimeStrings : TitleRelatedStrings {
-    override fun titleStatus(status: EntryStatus): Int = TODO("Not yet implemented")
+object AnimeStrings : DependsOnTitleStrings {
+    override fun titleStatusId(status: EntryStatus): Int = when(status) {
+        EntryStatus.Anons -> R.string.core_ui2_strings_anime_status_anons
+        EntryStatus.Ongoing -> R.string.core_ui2_strings_anime_status_ongoing
+        EntryStatus.Released -> R.string.core_ui2_strings_anime_status_released
+        EntryStatus.Paused -> R.string.core_ui2_strings_anime_status_paused
+        EntryStatus.Discontinued -> R.string.core_ui2_strings_anime_status_discontinued
+        EntryStatus.None -> R.string.core_ui2_strings_none
+    }
 
-    override fun userStatus(status: UserRateStatus): Int = TODO("Not yet implemented")
+    override fun userStatusId(status: UserRateStatus): Int = when (status) {
+        UserRateStatus.Planned -> R.string.core_ui2_strings_anime_user_status_status_planned
+        UserRateStatus.Watching -> R.string.core_ui2_strings_anime_user_status_watching
+        UserRateStatus.Rewatching -> R.string.core_ui2_strings_anime_user_status_rewatching
+        UserRateStatus.Completed -> R.string.core_ui2_strings_anime_user_status_completed
+        UserRateStatus.OnHold -> R.string.core_ui2_strings_anime_user_status_on_hold
+        UserRateStatus.Dropped -> R.string.core_ui2_strings_anime_user_status_dropped
+        UserRateStatus.None -> R.string.core_ui2_strings_none
+    }
+
+    fun kindId(kind: AnimeKind): Int = when(kind) {
+        AnimeKind.Tv -> R.string.core_ui2_strings_anime_kind_tv
+        AnimeKind.Movie -> R.string.core_ui2_strings_anime_kind_movie
+        AnimeKind.Ona -> R.string.core_ui2_strings_anime_kind_ona
+        AnimeKind.Ova -> R.string.core_ui2_strings_anime_kind_ova
+        AnimeKind.Music -> R.string.core_ui2_strings_anime_kind_music
+        AnimeKind.Special -> R.string.core_ui2_strings_anime_kind_special
+        AnimeKind.TvSpecial -> R.string.core_ui2_strings_anime_kind_tv_special
+        AnimeKind.Pv -> R.string.core_ui2_strings_anime_kind_pv
+        AnimeKind.Cm -> R.string.core_ui2_strings_anime_kind_cm
+        AnimeKind.None -> R.string.core_ui2_strings_none
+    }
+
+    fun ratingId(rating: AnimeRating): Int = when (rating) {
+        AnimeRating.G -> R.string.core_ui2_strings_anime_rating_g
+        AnimeRating.PG -> R.string.core_ui2_strings_anime_rating_pg
+        AnimeRating.PG13 -> R.string.core_ui2_strings_anime_rating_pg13
+        AnimeRating.R -> R.string.core_ui2_strings_anime_rating_r
+        AnimeRating.RPlus -> R.string.core_ui2_strings_anime_rating_r_plus
+        AnimeRating.RX -> R.string.core_ui2_strings_anime_rating_rx
+        AnimeRating.None -> R.string.core_ui2_strings_none
+    }
 }
