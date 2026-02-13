482f2cc7ce72db8737f290ae6e79da4f4f423cf0
saybu <saybulinv@gmail.com>
2026-02-13 00:09:58 +0000

    feat(core/ui2): add core:ui2:entry module

---
+++ core/ui2/entry/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.seanime.android.library)
+    alias(libs.plugins.seanime.android.library.compose)
+}
+
+android {
+    namespace = "ru.vladsaybulin.core.ui2.entry"
+}
+
+dependencies {
+    implementation(projects.core.model)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.ui2.strings)
+    implementation(projects.core.ui2.score)
+
+    implementation(libs.coil.kt.compose)
+}
