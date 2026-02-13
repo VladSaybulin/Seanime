ef7e57a45102c5d79c2e3a963a5219fb0f97ae51
saybu <saybulinv@gmail.com>
2026-02-13 00:16:17 +0000

    feat(core/ui2/entry): add AnimeGridItem and AnimeListItem components with default additional content

---
+++ core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/AnimeItem.kt
@@ -0,0 +1,147 @@
+package ru.vladsaybulin.ui2.entry
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import ru.vladsaybulin.core.designsystem.theme.SeanimeTheme
+import ru.vladsaybulin.core.ui2.strings.AnimeStrings
+import ru.vladsaybulin.core.ui2.strings.compose.ProvideTitleStringsByType
+import ru.vladsaybulin.core.ui2.strings.compose.asString
+import ru.vladsaybulin.core.ui2.strings.compose.asStringOrNull
+import ru.vladsaybulin.model.anime.Anime
+import ru.vladsaybulin.model.anime.AnimeKind
+import ru.vladsaybulin.model.common.EntryStatus
+import ru.vladsaybulin.model.common.EntryType
+import ru.vladsaybulin.model.userrate.UserRateStatus
+import ru.vladsaybulin.ui2.entry.preview.AnimeItemPreviewParameterProvider
+
+@Composable
+fun AnimeGridItem(
+    anime: Anime,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    userRateStatus: UserRateStatus = UserRateStatus.None,
+    style: EntryItemStyle = EntryItemDefaults.regularGridStyle(),
+    additionalContent: (@Composable () -> Unit)? = { AnimeGridItemDefaultAdditionalContent(anime) },
+) {
+    EntryGridItem(
+        name = anime.name,
+        russianName = anime.russianName,
+        poster = anime.poster,
+        onClick = onClick,
+        modifier = modifier,
+        userRateStatus = userRateStatus,
+        style = style,
+        additionalContent = additionalContent
+    )
+}
+
+@Composable
+fun AnimeListItem(
+    anime: Anime,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    userRateStatus: UserRateStatus = UserRateStatus.None,
+    posterWidth: Dp = EntryItemDefaults.listItemPosterWidth,
+    style: EntryItemStyle = EntryItemDefaults.regularListStyle(),
+    additionalContent: (@Composable () -> Unit)? = { AnimeListItemDefaultAdditionalContent(anime) },
+) {
+    EntryListItem(
+        name = anime.name,
+        russianName = anime.russianName,
+        poster = anime.poster,
+        onClick = onClick,
+        modifier = modifier,
+        userRateStatus = userRateStatus,
+        posterWidth = posterWidth,
+        style = style,
+        additionalContent = additionalContent
+    )
+}
+
+@Composable
+fun AnimeGridItemDefaultAdditionalContent(anime: Anime) {
+    TitleGridItemAdditionalContent(
+        kindStr = anime.kind.asStringOrNull(),
+        year = anime.airedOn?.year
+    )
+}
+
+@Composable
+fun AnimeListItemDefaultAdditionalContent(anime: Anime) {
+    TitleListItemDefaultAdditionalContent(
+        status = anime.status,
+        year = anime.airedOn?.year ?: anime.releasedOn?.year,
+        kindAndVolumes = kindAndEpisodesFormatted(
+            kind = anime.kind,
+            episodes = anime.episodes,
+            episodesAired = anime.episodesAired,
+            status = anime.status
+        ),
+        score = anime.score
+    )
+}
+
+@Composable
+@ReadOnlyComposable
+private fun kindAndEpisodesFormatted(
+    kind: AnimeKind,
+    episodes: Int,
+    episodesAired: Int,
+    status: EntryStatus
+): String? {
+    val kindStr = kind.asStringOrNull()
+    val episodesStr =
+        AnimeStrings.getProgressFormat(
+            aired = episodesAired,
+            total = episodes,
+            isOngoing = status == EntryStatus.Ongoing,
+            isMovie = kind == AnimeKind.Movie
+        )?.asString()
+
+    val layoutDirection = LocalLayoutDirection.current
+
+    val finalStr = listOfNotNull(kindStr, episodesStr).apply {
+        if (layoutDirection == LayoutDirection.Rtl) {
+            reversed()
+        }
+    }.joinToString(separator = " • ")
+
+    return finalStr.takeIf { it.isNotEmpty() }
+}
+
+@Preview
+@Composable
+private fun AnimeListItemPreview(
+    @PreviewParameter(AnimeItemPreviewParameterProvider::class) anime: Anime
+) {
+    SeanimeTheme {
+        ProvideTitleStringsByType(EntryType.Anime) {
+            AnimeListItem(
+                anime = anime,
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun AnimeGridItemPreview(@PreviewParameter(AnimeItemPreviewParameterProvider::class) anime: Anime) {
+    SeanimeTheme {
+        ProvideTitleStringsByType(EntryType.Anime) {
+            AnimeGridItem(
+                modifier = Modifier.width(150.dp),
+                anime = anime,
+                onClick = {}
+            )
+        }
+    }
+}
---
+++ core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/DefaultAdditionalContent.kt
@@ -0,0 +1,189 @@
+package ru.vladsaybulin.ui2.entry
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.vladsaybulin.core.designsystem.components.SeanimeTag
+import ru.vladsaybulin.core.designsystem.components.TagDefaults
+import ru.vladsaybulin.core.designsystem.theme.SeanimeTheme
+import ru.vladsaybulin.core.designsystem.theme.get
+import ru.vladsaybulin.core.ui2.score.Score
+import ru.vladsaybulin.core.ui2.strings.compose.ProvideTitleStringsByType
+import ru.vladsaybulin.core.ui2.strings.compose.asString
+import ru.vladsaybulin.model.common.EntryStatus
+import ru.vladsaybulin.model.common.EntryType
+
+@Composable
+fun TitleGridItemAdditionalContent(
+    kindStr: String?,
+    year: Int?
+) {
+    val finalText = listOfNotNull(kindStr, year?.toString()).joinToString(separator = " • ")
+    Text(finalText)
+}
+
+@Composable
+fun TitleListItemDefaultAdditionalContent(
+    status: EntryStatus,
+    year: Int?,
+    kindAndVolumes: String?,
+    score: Float = 0f,
+) {
+
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            StatusTag(status)
+            if (year != null) {
+                Text(text = year.toString())
+            }
+        }
+        if (kindAndVolumes != null) {
+            Text(text = kindAndVolumes)
+        }
+        if (score > 0) {
+            Score(
+                value = score,
+                iconSize = 16.dp,
+                numberStyle = LocalTextStyle.current
+            )
+        }
+    }
+}
+
+@Composable
+private fun StatusTag(status: EntryStatus) {
+    if (status != EntryStatus.None) {
+        val color = SeanimeTheme.seanimeColors[status]
+        SeanimeTag(
+            border = TagDefaults.border(color = color),
+            contentColor = color,
+            modifier = Modifier.padding(StatusTagPadding)
+        ) {
+            Text(status.asString())
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_Full() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.Ongoing,
+                    year = 2024,
+                    kindAndVolumes = "TV, 12 eps"
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_WithoutStatus() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.None,
+                    year = 2024,
+                    kindAndVolumes = "TV, 12 eps"
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_WithoutYear() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.Ongoing,
+                    year = null,
+                    kindAndVolumes = "TV, 12 eps"
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_WithoutKindAndVolumes() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.Ongoing,
+                    year = 2024,
+                    kindAndVolumes = null
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_StatusOnly() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.Ongoing,
+                    year = null,
+                    kindAndVolumes = null
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_YearOnly() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.None,
+                    year = 2024,
+                    kindAndVolumes = null
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TitleListItemAdditionalContentMeasurePolicy_KindAndEpisodesOnly() {
+    SeanimeTheme {
+        Surface(color = SeanimeTheme.colorScheme.surfaceContainer) {
+            ProvideTitleStringsByType(EntryType.Anime) {
+                TitleListItemDefaultAdditionalContent(
+                    status = EntryStatus.None,
+                    year = null,
+                    kindAndVolumes = "TV, 12 eps"
+                )
+            }
+        }
+    }
+}
+
+private val StatusSpace = 4.dp
+private val StatusTagPadding = PaddingValues(top = StatusSpace, end = StatusSpace, bottom = StatusSpace)
