fb0d143bcfe4734730efe1a8f5f33d93cf7c220e
saybu <saybulinv@gmail.com>
2026-02-16 23:02:32 +0000

    feat: add MangaItem, MangaGrid and MangaList components for displaying manga items

---
+++ core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/manga/MangaList.kt
@@ -0,0 +1,68 @@
+package ru.vladsaybulin.ui2.entry.manga
+
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ru.vladsaybulin.model.manga.Manga
+import ru.vladsaybulin.model.userrate.UserRateStatus
+import ru.vladsaybulin.ui2.entry.EntryItemDefaults
+import ru.vladsaybulin.ui2.entry.EntryItemStyle
+
+inline fun LazyListScope.mangaItems(
+    mangas: List<Manga>,
+    crossinline onItemClick: (Manga) -> Unit,
+    noinline key: ((Manga) -> Any)? = null,
+    noinline contentType: (Manga) -> Any? = { null },
+    itemModifier: Modifier = Modifier,
+    style: EntryItemStyle? = null,
+    crossinline userRateStatus: (Manga) -> UserRateStatus = { UserRateStatus.None },
+    noinline additionalContent: (@Composable (Manga) -> Unit)? = { MangaListItemDefaultAdditionalContent(it) },
+) {
+    items(
+        count = mangas.size,
+        key = if (key != null) {
+            { index -> key(mangas[index]) }
+        } else null,
+        contentType = { index -> contentType(mangas[index]) }
+    ) { index ->
+        val manga = mangas[index]
+        MangaListItem(
+            manga = manga,
+            onClick = { onItemClick(manga) },
+            modifier = itemModifier,
+            userRateStatus = userRateStatus(manga),
+            style = style ?: EntryItemDefaults.regularListStyle(),
+            additionalContent = { additionalContent?.invoke(manga) }
+        )
+    }
+}
+
+inline fun LazyListScope.mangaCarouselItems(
+    mangas: List<Manga>,
+    crossinline onItemClick: (Manga) -> Unit,
+    noinline key: ((Manga) -> Any)? = null,
+    noinline contentType: (Manga) -> Any? = { null },
+    itemModifier: Modifier = Modifier,
+    style: EntryItemStyle? = null,
+    crossinline userRateStatus: (Manga) -> UserRateStatus = { UserRateStatus.None },
+    noinline additionalContent: (@Composable (Manga) -> Unit)? = { MangaGridItemDefaultAdditionalContent(it) },
+) {
+    items(
+        count = mangas.size,
+        key = if (key != null) {
+            { index -> key(mangas[index]) }
+        } else null,
+        contentType = { index -> contentType(mangas[index]) }
+    ) { index ->
+        val manga = mangas[index]
+        MangaGridItem(
+            manga = manga,
+            onClick = { onItemClick(manga) },
+            modifier = itemModifier,
+            userRateStatus = userRateStatus(manga),
+            style = style ?: EntryItemDefaults.regularGridStyle(),
+            additionalContent = { additionalContent?.invoke(manga) }
+        )
+    }
+}
+
