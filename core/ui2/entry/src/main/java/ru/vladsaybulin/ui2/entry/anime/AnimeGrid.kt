b86c64ba558b1aa1108180284921e06d1942bab7
saybu <saybulinv@gmail.com>
2026-02-17 01:07:15 +0000

    feat: add LazyPagingItems support for anime and manga in grid and list components

--- core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/anime/AnimeGrid.kt
+++ core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/anime/AnimeGrid.kt
@@ -3,6 +3,9 @@ package ru.vladsaybulin.ui2.entry.anime
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import ru.vladsaybulin.model.anime.Anime
 import ru.vladsaybulin.model.userrate.UserRateStatus
 import ru.vladsaybulin.ui2.entry.EntryItemDefaults
@@ -34,5 +37,34 @@ inline fun LazyGridScope.animeItems(
             style = style ?: EntryItemDefaults.regularGridStyle(),
             additionalContent = { additionalContent?.invoke(anime) }
         )
+    }
+}
+
+inline fun LazyGridScope.animeItems(
+    animes: LazyPagingItems<Anime>,
+    crossinline onItemClick: (Anime) -> Unit,
+    noinline key: ((Anime) -> Any)? = { it.id },
+    noinline contentType: (item: Anime) -> Any? = { null },
+    itemModifier: Modifier = Modifier,
+    style: EntryItemStyle? = null,
+    crossinline userRateStatus: (Anime) -> UserRateStatus = { UserRateStatus.None },
+    noinline additionalContent: (@Composable (Anime) -> Unit)? = { AnimeGridItemDefaultAdditionalContent(it) },
+) {
+    items(
+        count = animes.itemCount,
+        key = animes.itemKey(key),
+        contentType = animes.itemContentType(contentType),
+    ) {
+        val anime = animes[it]
+        if (anime != null) {
+            AnimeGridItem(
+                anime = anime,
+                onClick = { onItemClick(anime) },
+                modifier = itemModifier,
+                userRateStatus = userRateStatus(anime),
+                style = style ?: EntryItemDefaults.regularGridStyle(),
+                additionalContent = { additionalContent?.invoke(anime) }
+            )
+        }
     }
 }
--- core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/anime/AnimeList.kt
+++ core/ui2/entry/src/main/java/ru/vladsaybulin/ui2/entry/anime/AnimeList.kt
@@ -1,8 +1,12 @@
 package ru.vladsaybulin.ui2.entry.anime
 
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import ru.vladsaybulin.model.anime.Anime
 import ru.vladsaybulin.model.userrate.UserRateStatus
 import ru.vladsaybulin.ui2.entry.EntryItemDefaults
@@ -34,6 +38,35 @@ inline fun LazyListScope.animeItems(
             style = style ?: EntryItemDefaults.regularListStyle(),
             additionalContent = { additionalContent?.invoke(anime) }
         )
+    }
+}
+
+inline fun LazyListScope.animeItems(
+    animes: LazyPagingItems<Anime>,
+    crossinline onItemClick: (Anime) -> Unit,
+    noinline key: ((Anime) -> Any)? = { it.id },
+    noinline contentType: (item: Anime) -> Any? = { null },
+    itemModifier: Modifier = Modifier,
+    style: EntryItemStyle? = null,
+    crossinline userRateStatus: (Anime) -> UserRateStatus = { UserRateStatus.None },
+    noinline additionalContent: (@Composable (Anime) -> Unit)? = { AnimeListItemDefaultAdditionalContent(it) },
+) {
+    items(
+        count = animes.itemCount,
+        key = animes.itemKey(key),
+        contentType = animes.itemContentType(contentType),
+    ) {
+        val anime = animes[it]
+        if (anime != null) {
+            AnimeListItem(
+                anime = anime,
+                onClick = { onItemClick(anime) },
+                modifier = itemModifier,
+                userRateStatus = userRateStatus(anime),
+                style = style ?: EntryItemDefaults.regularGridStyle(),
+                additionalContent = { additionalContent?.invoke(anime) }
+            )
+        }
     }
 }
 
