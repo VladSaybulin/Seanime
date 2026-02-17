21851627975604a4a5cb3a6bc420ad91c04b56cf
saybu <saybulinv@gmail.com>
2026-02-17 00:37:55 +0000

    feat(feature/calendar): integrate EntryCarousel into CalendarScreen

--- feature/calendar/build.gradle.kts
+++ feature/calendar/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation(projects.core.data)
     implementation(projects.core.domain)
     implementation(projects.core.navigation)
+    implementation(projects.core.ui2.entry)
 
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
