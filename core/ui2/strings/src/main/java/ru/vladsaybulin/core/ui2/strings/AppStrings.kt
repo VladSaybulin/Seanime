5e65c763d3b1fd7e320c260106e5f6a294e3a742
saybu <saybulinv@gmail.com>
2026-04-26 15:20:18 +0000

    fix: correct string resource for Manga title type

--- core/ui2/strings/src/main/java/ru/vladsaybulin/core/ui2/strings/AppStrings.kt
+++ core/ui2/strings/src/main/java/ru/vladsaybulin/core/ui2/strings/AppStrings.kt
@@ -5,6 +5,6 @@ import ru.vladsaybulin.model.common.EntryType
 object AppStrings {
     fun titleType(entryType: EntryType): Int = when (entryType) {
         EntryType.Anime -> R.string.core_ui2_strings_title_type_anime
-        EntryType.Manga -> R.string.core_ui2_strings_title_type_anime
+        EntryType.Manga -> R.string.core_ui2_strings_title_type_manga
     }
 }
