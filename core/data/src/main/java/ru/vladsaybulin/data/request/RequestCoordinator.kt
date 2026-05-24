335c92e74041cf19cecb7f3b2c0adfbe103ea6bd
saybu <saybulinv@gmail.com>
2026-05-24 09:46:24 +0000

    feat(core/data): add RequestCoordinator for managing request deduplication and synchronization

---
+++ core/data/src/main/java/ru/vladsaybulin/data/request/RequestCoordinator.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 Vlad Saybulin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.vladsaybulin.data.request
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import ru.vladsaybulin.common.network.Dispatcher
+import ru.vladsaybulin.common.network.ShikiDispatchers
+import javax.inject.Inject
+
+class RequestCoordinator @Inject constructor(
+    private val deduplicator: RequestDeduplicator,
+    private val syncer: RequestSyncer,
+    @param:Dispatcher(ShikiDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend fun <T> request(
+        key: RequestKey,
+        block: suspend () -> T
+    ) = withContext(ioDispatcher) {
+        deduplicator.request(key, block)
+    }
+
+    suspend fun sync(
+        key: RequestKey.Cached,
+        forceRefresh: Boolean,
+        ttlStrategy: TTLStrategy,
+        block: suspend UpdateScope.() -> Unit
+    ) {
+        request(key) {
+            syncer.sync(key, forceRefresh, ttlStrategy, block)
+        }
+    }
+}
