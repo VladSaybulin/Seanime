8868102aa0a01dde74f0a89e5e36983aadc7a30d
saybu <saybulinv@gmail.com>
2026-09-10 21:56:24 +0300

    refactor(core/domain): refactor GetPaged*SearchUseCase to use AuthRepository for session state management

--- core/domain/src/main/java/ru/vladsaybulin/core/domain/search/GetPagedAnimeSearchUseCase.kt
+++ core/domain/src/main/java/ru/vladsaybulin/core/domain/search/GetPagedAnimeSearchUseCase.kt
@@ -19,24 +19,23 @@ package ru.vladsaybulin.core.domain.search
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
-import ru.vladsaybulin.core.auth.ShikimoriAuthorization
 import ru.vladsaybulin.core.domain.repository.AnimeRepository
+import ru.vladsaybulin.core.domain.repository.AuthRepository
 import ru.vladsaybulin.model.anime.Anime
 import ru.vladsaybulin.model.search.QueryMapKey
 import javax.inject.Inject
 
 class GetPagedAnimeSearchUseCase @Inject internal constructor(
     private val animeRepository: AnimeRepository,
-    private val shikimoriAuthorization: ShikimoriAuthorization
+    private val authRepository: AuthRepository
 ) {
     operator fun invoke(
         queryMap: Map<QueryMapKey, String>,
         pagingConfig: PagingConfig = DefaultSearchPagingConfig
     ): Flow<PagingData<Anime>> = pagedSearch(
-        authStateFlow = shikimoriAuthorization.shikimoriAuthState,
+        authStateFlow = authRepository.sessionState,
         config = pagingConfig
     ) {
         animeRepository.animeSearchPagingSource(queryMap)
     }
 }
-
