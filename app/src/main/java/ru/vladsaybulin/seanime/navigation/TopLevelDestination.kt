4c1118eb3e8478ffc5c87ab1bbfddfe8d36b22a8
saybu <saybulinv@gmail.com>
2026-09-14 11:43:00 +0300

    feat(app): update TopLevelDestination to use navKey for navigation

--- app/src/main/java/ru/vladsaybulin/seanime/navigation/TopLevelDestination.kt
+++ app/src/main/java/ru/vladsaybulin/seanime/navigation/TopLevelDestination.kt
@@ -18,45 +18,46 @@ package ru.vladsaybulin.seanime.navigation
 
 import androidx.compose.ui.graphics.vector.ImageVector
 import ru.vladsaybulin.core.designsystem.icons.SeanimeIcons
-import ru.vladsaybulin.feature.calendar.navigation.CalendarGraphRoute
-import ru.vladsaybulin.feature.home.navigation.HomeGraphRoute
-import ru.vladsaybulin.feature.list.navigation.ListGraphRoute
-import ru.vladsaybulin.feature.search.navigation.SearchGraphRoute
+import ru.vladsaybulin.core.navigation.SeanimeNavKey
+import ru.vladsaybulin.feature.calendar.navigation.CalendarNavKey
+import ru.vladsaybulin.feature.home.navigation.HomeNavKey
+import ru.vladsaybulin.feature.list.navigation.ListNavKey
+import ru.vladsaybulin.feature.search.navigation.SearchNavKey
 import ru.vladsaybulin.feature.calendar.R as calendarR
 import ru.vladsaybulin.feature.home.R as homeR
 import ru.vladsaybulin.feature.list.R as listR
 import ru.vladsaybulin.feature.search.R as searchR
 
 enum class TopLevelDestination(
-    val graphRoute: Any,
+    val navKey: SeanimeNavKey,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
     val iconTextId: Int,
     val titleTextId: Int,
 ) {
     HOME(
-        graphRoute = HomeGraphRoute,
+        navKey = HomeNavKey,
         selectedIcon = SeanimeIcons.Home,
         unselectedIcon = SeanimeIcons.OutlinedHome,
         iconTextId = homeR.string.feature_home_title,
         titleTextId = homeR.string.feature_home_title
     ),
     SEARCH(
-        graphRoute = SearchGraphRoute,
+        navKey = SearchNavKey(),
         selectedIcon = SeanimeIcons.Search,
         unselectedIcon = SeanimeIcons.Search,
         iconTextId = searchR.string.feature_search_title,
         titleTextId = searchR.string.feature_search_title
     ),
     LIST(
-        graphRoute = ListGraphRoute,
+        navKey = ListNavKey(),
         selectedIcon = SeanimeIcons.Bookmark,
         unselectedIcon = SeanimeIcons.OutlinedBookmark,
         iconTextId = listR.string.feature_list_title,
         titleTextId = listR.string.feature_list_title,
     ),
     CALENDAR(
-        graphRoute = CalendarGraphRoute,
+        navKey = CalendarNavKey,
         selectedIcon = SeanimeIcons.CalendarToday,
         unselectedIcon = SeanimeIcons.CalendarToday,
         iconTextId = calendarR.string.feature_calendar_title,
