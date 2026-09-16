38542ce6e2139d2a066f9b6485892eaf66bba8a5
saybu <saybulinv@gmail.com>
2026-09-16 16:51:36 +0300

    feat(feature/title/videos): refactor AnimeVideosViewModel to use AnimeVideosNavKey for navigation

--- feature/title/videos/impl/src/main/kotlin/ru/vladsaybulin/feature/title/videos/impl/AnimeVideosViewModel.kt
+++ feature/title/videos/impl/src/main/kotlin/ru/vladsaybulin/feature/title/videos/impl/AnimeVideosViewModel.kt
@@ -18,27 +18,31 @@ package ru.vladsaybulin.feature.title.videos.impl
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.toRoute
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import ru.vladsaybulin.data.repository.AnimeRepository
-import ru.vladsaybulin.feature.title.videos.navigation.AnimeVideosScreenRoute
+import ru.vladsaybulin.feature.title.videos.api.navigation.AnimeVideosNavKey
 import ru.vladsaybulin.model.anime.Video
 import javax.inject.Inject
 
 @HiltViewModel
 class AnimeVideosViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-    animeRepository: AnimeRepository
+    animeRepository: AnimeRepository,
+    @Assisted key: AnimeVideosNavKey
 ) : ViewModel() {
-    private val route: AnimeVideosScreenRoute = savedStateHandle.toRoute()
 
-    internal val uiState = animeRepository.getAllAnimeVideos(route.animeId)
+    @AssistedFactory
+    interface Factory {
+        fun create(key: AnimeVideosNavKey): AnimeVideosViewModel
+    }
+
+    internal val uiState = animeRepository.getAllAnimeVideos(key.animeId)
         .map { AnimeVideosUIState.Success(it) }
         .stateIn(
             scope = viewModelScope,
