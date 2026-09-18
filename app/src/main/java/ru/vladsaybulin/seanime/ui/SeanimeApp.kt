7bf389e7dfdf0942f20d180dee6ca9c70d02f64a
saybu <saybulinv@gmail.com>
2026-09-18 22:01:09 +0300

    feat(app): enable imageViewEntry in navigation

--- app/src/main/java/ru/vladsaybulin/seanime/ui/SeanimeApp.kt
+++ app/src/main/java/ru/vladsaybulin/seanime/ui/SeanimeApp.kt
@@ -46,6 +46,7 @@ import ru.vladsaybulin.feature.character.impl.navigation.characterEntry
 import ru.vladsaybulin.feature.home.impl.navigation.homeEntry
 import ru.vladsaybulin.feature.imageview.impl.FullScreenImageState
 import ru.vladsaybulin.feature.imageview.impl.FullScreenImageView
+import ru.vladsaybulin.feature.imageview.impl.navigation.imageViewEntry
 import ru.vladsaybulin.feature.list.impl.navigation.listEntry
 import ru.vladsaybulin.feature.profile.impl.navigation.profileEntry
 import ru.vladsaybulin.feature.search.impl.navigation.searchEntry
@@ -113,7 +114,7 @@ fun SeanimeApp(
                         characterEntry()
                         profileEntry()
 
-                        //imageViewEntry()
+                        imageViewEntry()
                         //rateEditorEntry()
                     }
                 }
