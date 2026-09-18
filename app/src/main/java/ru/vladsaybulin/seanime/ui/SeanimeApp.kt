a95f61576c1358dec6ab6883fffca5cbe74abbc3
saybu <saybulinv@gmail.com>
2026-09-18 22:16:52 +0300

    refactor: remove FullScreenImageView components and clear it from SeanimeApp

--- app/src/main/java/ru/vladsaybulin/seanime/ui/SeanimeApp.kt
+++ app/src/main/java/ru/vladsaybulin/seanime/ui/SeanimeApp.kt
@@ -44,8 +44,6 @@ import ru.vladsaybulin.core.designsystem.theme.SeanimeTheme
 import ru.vladsaybulin.feature.calendar.impl.navigation.calendarEntry
 import ru.vladsaybulin.feature.character.impl.navigation.characterEntry
 import ru.vladsaybulin.feature.home.impl.navigation.homeEntry
-import ru.vladsaybulin.feature.imageview.impl.FullScreenImageState
-import ru.vladsaybulin.feature.imageview.impl.FullScreenImageView
 import ru.vladsaybulin.feature.imageview.impl.navigation.imageViewEntry
 import ru.vladsaybulin.feature.list.impl.navigation.listEntry
 import ru.vladsaybulin.feature.profile.impl.navigation.profileEntry
@@ -64,7 +62,6 @@ fun SeanimeApp(
     appState: SeanimeAppState
 ) {
     SeanimeTheme {
-        val fullScreenImageState = remember { FullScreenImageState() }
 
         var editableUserRate by remember {
             mutableStateOf<EditableUserRate?>(null)
@@ -137,13 +134,6 @@ fun SeanimeApp(
             //    editableUserRate = checkNotNull(editableUserRate),
             //    onDismissRequest = { editableUserRate = null }
             //)
-        }
-
-        if (fullScreenImageState.isVisible) {
-            FullScreenImageView(
-                state = fullScreenImageState,
-                onDismissRequest = { }
-            )
         }
     }
 }
